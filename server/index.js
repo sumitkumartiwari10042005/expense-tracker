f715c55745f386ff6324412f703be997cf1599c6
sumitkumartiwari10042005 <148178735+sumitkumartiwari10042005@users.noreply.github.com>
2026-04-28 06:17:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,3 +30,11 @@ app.use("/expenses", expensesRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
