314b0451e374d44b71ed4f92b0445ade2c236cce
sumitkumartiwari10042005 <148178735+sumitkumartiwari10042005@users.noreply.github.com>
2026-04-28 06:41:10 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,15 @@
 import sqlite3 from "sqlite3";
-import fs from "fs";
+import { readFileSync } from "fs";
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-const db = new sqlite3.Database("./expenses.db");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// run schema once
-const schema = fs.readFileSync("./schema.sql", "utf-8");
+const db = new sqlite3.Database(path.join(__dirname, 'expenses.db'));
+
+const schema = readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
+
 db.exec(schema);
 
 export default db;
