43060a15ea38ddf1c6050b8965af70756173c56e
sumitkumartiwari10042005 <148178735+sumitkumartiwari10042005@users.noreply.github.com>
2026-05-03 19:43:22 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export async function getExpenses() {
   const res = await fetch(`${BASE_URL}/expenses`);
