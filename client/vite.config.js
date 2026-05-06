a61701690f965e959c93a4d1e8d29691d489f49c
kumar <kumarsumit10042005@gmail.com>
2026-05-06 22:24:04 +0530

    changed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +14,8 @@ export default defineConfig({
       "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
