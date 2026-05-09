0f276b9ab049d10f10ef7e7bdc90f18ad5613379
kumar <kumarsumit10042005@gmail.com>
2026-05-09 23:54:30 +0530

    updated server/index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { initDB } from './db.js';
 import authRoutes from './routes/auth.js';
 import expenseRoutes from './routes/expenses.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production'
+    ? 'https://expense-tracker-1-cde9.onrender.com'
+    : 'http://localhost:5173',
   credentials: true
 }));
 
@@ -19,10 +25,15 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-const PORT = process.env.PORT || 3000;
+// Production me React serve karo
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+  });
+}
 
+const PORT = process.env.PORT || 3000;
 initDB().then(() =>
-  app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}`)
-  )
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 );
